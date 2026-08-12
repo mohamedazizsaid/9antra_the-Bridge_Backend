a7db2db16fab1f7c56c2b8fff426ac9373a75baa
aziz <saidazizz132@gmail.com>
2026-08-12 14:22:07 +0100

    settings profile fix

--- src/main/java/com/_antra/the_bridge/entity/User.java
+++ src/main/java/com/_antra/the_bridge/entity/User.java
@@ -22,6 +22,7 @@ public class User {
     private int id;
     private String firstName;
     private String lastName;
+    @Column(columnDefinition = "LONGTEXT")
     private String avatar;
     private int age;
     private String email;
